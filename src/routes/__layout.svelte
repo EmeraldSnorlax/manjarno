1588f5f7ee30d3c4d420e7983246eb8cf504976a
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-01-03 16:35:44 +0000

    transcribe old page and minor tweaks

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,8 @@
 	import '../app.postcss';
 </script>
 
-<main class="max-w-prose flex mx-auto px-6 mt-6">
-  <article class="prose dark:prose-invert">
+<main class="max-w-prose flex mx-auto px-6 my-12">
+  <article class="prose prose-stone dark:prose-invert">
     <slot/>
   </article>
 </main>
