b59a596dff690606b64843d0b259173292957e85
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-01-03 16:03:04 +0000

    dont collide on the edge

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.postcss';
 </script>
 
-<main class="max-w-prose flex mx-auto mt-6">
+<main class="max-w-prose flex mx-auto px-6 mt-6">
   <article class="prose dark:prose-invert">
     <slot/>
   </article>
