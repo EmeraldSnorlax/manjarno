2f77013f01f569e95eecb884d79b8e1f023f9946
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-01-03 18:06:32 +0000

    prettier

--- src/routes/Stopwatch.svelte
+++ src/routes/Stopwatch.svelte
@@ -33,18 +33,21 @@
 
 <div class="w-full flex items-center justify-center mb-32">
 	<div>
-    <noscript class="text-center inline-block w-full text-red-600">
-      Enable JavaScript to see the stopwatch.
-    </noscript>
+		<noscript class="text-center inline-block w-full text-red-600">
+			Enable JavaScript to see the stopwatch.
+		</noscript>
 		<span class="block text-2xl text-center">It has been</span>
 		<span class="font-display block text-5xl text-center my-2"
-			>{days}d {hours}h <span class="hidden md:inline-block">{minutes}m</span> <span class="hidden lg:inline-block">{seconds}s</span>
-      </span>
+			>{days}d {hours}h <span class="hidden md:inline-block">{minutes}m</span>
+			<span class="hidden lg:inline-block">{seconds}s</span>
+		</span>
 		<span class="block text-xl text-center"
 			>since Manjaro <span class="inline-block font-display">!$%&?*#</span> up.</span
 		>
 		<span class="block">
-			On <span class="inline-block font-display">{event.date.getFullYear()}-{pad(event.date.getMonth(), 2)}-{pad(event.date.getDate(), 2)} </span>, they {event.reason}
+			On <span class="inline-block font-display"
+				>{event.date.getFullYear()}-{pad(event.date.getMonth(), 2)}-{pad(event.date.getDate(), 2)}
+			</span>, they {event.reason}
 		</span>
 	</div>
 </div>
