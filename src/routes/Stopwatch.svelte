3de4bcccccb0d830584d5b21bfebd84113dc88ac
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-12-13 15:04:36 +0000

    migrate to new sveltekit and make stopwatch SSR

--- src/routes/Stopwatch.svelte
+++ src/routes/Stopwatch.svelte
@@ -15,27 +15,25 @@
 
 		return str;
 	}
-	let days = '??';
-	let hours = '??';
-	let minutes = '??';
-	let seconds = '??';
+
+	const currentTime = new Date().getTime() - new Date(event.date).getTime();
+	let days = String(Math.floor(currentTime / (1000 * 60 * 60 * 24)));
+	let hours = pad(Math.floor(currentTime / (1000 * 60 * 60)) % 24, 2);
+	let minutes = pad(Math.floor(currentTime / (1000 * 60)) % 60, 2);
+	let seconds = pad(Math.floor(currentTime / 1000) % 60, 2);
 
 	onMount(() => {
 		setInterval(() => {
-			const time = new Date().getTime() - new Date(event.date).getTime();
-			days = String(Math.floor(time / (1000 * 60 * 60 * 24)));
-			hours = pad(Math.floor(time / (1000 * 60 * 60)) % 24, 2);
-			minutes = pad(Math.floor(time / (1000 * 60)) % 60, 2);
-			seconds = pad(Math.floor(time / 1000) % 60, 2);
+			days = String(Math.floor(currentTime / (1000 * 60 * 60 * 24)));
+			hours = pad(Math.floor(currentTime / (1000 * 60 * 60)) % 24, 2);
+			minutes = pad(Math.floor(currentTime / (1000 * 60)) % 60, 2);
+			seconds = pad(Math.floor(currentTime / 1000) % 60, 2);
 		}, 200);
 	});
 </script>
 
 <div class="w-full flex items-center justify-center mb-32">
 	<div>
-		<noscript class="text-center inline-block w-full text-red-600">
-			Enable JavaScript to see the stopwatch.
-		</noscript>
 		<span class="block text-2xl text-center">It has been</span>
 		<span class="font-display block text-5xl text-center my-2"
 			>{days}d {hours}h <span class="hidden md:inline-block">{minutes}m</span>
@@ -46,7 +44,10 @@
 		>
 		<span class="block">
 			On <span class="inline-block font-display"
-				>{event.date.getFullYear()}-{pad(event.date.getMonth() + 1, 2)}-{pad(event.date.getDate(), 2)}
+				>{event.date.getFullYear()}-{pad(event.date.getMonth() + 1, 2)}-{pad(
+					event.date.getDate(),
+					2
+				)}
 			</span>, they {event.reason}
 		</span>
 	</div>
